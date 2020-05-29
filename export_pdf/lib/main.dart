0f536c54f97c7143c9a3355362035906b343b9d0
Henrique Mello <henriquemello@MacBook-Pro-de-Henrique.local>
2020-05-29 18:33:57 -0300

    Aplicação de exemplo para exportar para documento PDF paginado com exibição integrada.

---
+++ export_pdf/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:export_pdf/montaPDF.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'PDF Generator & Viewer'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Pressione o botão para gerar o PDF',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          montaPDF(context);
+        },
+        tooltip: 'Gerar PDF',
+        child: Icon(Icons.picture_as_pdf),
+      ),
+    );
+  }
+}
